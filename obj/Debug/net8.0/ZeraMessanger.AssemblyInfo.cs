ecc406eeffa4d43a1efe36bbfef606f19fbd088d
Artem Chekhonin <chekhonin.artem@gmail.com>
2024-05-15 16:49:38 +0300

    Добавлен .gitignore

--- obj/Debug/net8.0/ZeraMessanger.AssemblyInfo.cs
+++ obj/Debug/net8.0/ZeraMessanger.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("ZeraMessanger")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+586e931cda1810cc898f44bab90db857600ba376")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+54e217bf3612c6be6245692c1af0382d7ca262be")]
 [assembly: System.Reflection.AssemblyProductAttribute("ZeraMessanger")]
 [assembly: System.Reflection.AssemblyTitleAttribute("ZeraMessanger")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
